310ccde3653686e3918092f85be1e53cbbe614bb
nmojicaa <nataliamoj13@gmail.com>
2023-02-23 21:31:50 -0500

    Update index.js
    index.js created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Weather from "./Weather";
+
+import "./styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Weather />
+    </div>
   </React.StrictMode>
 );
 
