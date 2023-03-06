8febe1d9d2116cb82b2cb91457b6aacd769ac0eb
nmojicaa <nataliamoj13@gmail.com>
2023-03-05 20:45:51 -0500

    Create WeatherInfo.js
    Weather info created

---
+++ src/WeatherInfo.js
@@ -0,0 +1,32 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <span className="units">
+              <strong>{props.temperature}</strong>
+              <a href="/"> °C</a> | <a href="/">°F</a>
+            </span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind}km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
