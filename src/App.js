e565213b18b34b7594d3d212b2125374003601c4
nmojicaa <nataliamoj13@gmail.com>
2023-03-07 12:33:53 -0500

    Update App.js
    referrer misspelled

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ export default function App() {
           <a
             href="https://github.com/nmojicaa/weather-react"
             target="_blank"
-            className="footer"
-            rel="norefferrer"
+            rel="noreferrer"
           >
             open-sourced on Guthub
           </a>
