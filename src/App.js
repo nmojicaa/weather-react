ffd877eec4bc3608a48239dc206f569ada987953
nmojicaa <nataliamoj13@gmail.com>
2023-03-06 21:27:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          This project was coded by Nataia Mojica and is open-sourced on Guthub
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
